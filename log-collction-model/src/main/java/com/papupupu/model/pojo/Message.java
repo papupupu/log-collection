127ec47f84265c6a0445fd5a46ac393afe90eb90
papupupu <zx2910935654@gmail.com>
2024-02-20 12:14:28 +0800

    通用result实现初步完成

---
+++ log-collction-model/src/main/java/com/papupupu/model/pojo/Message.java
@@ -0,0 +1,5 @@
+package com.papupupu.model.pojo;
+
+public class Message {
+
+}
---
+++ log-collection-common/src/main/java/com/paupupu/common/constants/Result/CodeConstants.java
@@ -0,0 +1,6 @@
+package com.paupupu.common.constants.Result;
+
+public class CodeConstants {
+    public static Integer SUCCESS = 200;
+
+}
